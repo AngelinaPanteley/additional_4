31728e18855235a45b500de3864830ea42e64ac3
Angelina <panteley-00@mail.ru>
2017-09-22 01:37:17 +0300

    bigdecimal

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+//var Big = require('big.js');
+//Big.E_POS = 100;
+//Big.DP = 100;
 module.exports = function multiply(first, second) {
-  // your solution
+var bigdecimal = require("bigdecimal");
+//function multiply(first, second) {
+  //var x=new Big(+first);
+  //var y=new Big(+second);
+  //var z=new Big(x*y);
+  //return z.toString();
+  var x = new bigdecimal.BigDecimal(+first);
+  var y = new bigdecimal.BigDecimal(+second);
+  var z = bigdecimal.BigDecimal(0);
+  z = x.multiply(y);
+  return z.toBigInteger().toString();
 }
+
+//multiply('329568934658432659586', '58379426534596');
